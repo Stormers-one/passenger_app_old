1dec32231a7161bf4421ac478217123eb1ec5654
kenn850 <kennith.philips@gmail.com>
2020-03-19 11:08:22 +0530

    Aded the key
    Corrected the previous crrashes

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -18,7 +18,7 @@ class Homepage extends StatelessWidget {
       return showDialog(
           context: context,
           builder: (context) => AlertDialog(
-                title: Text("Are you sure?"),
+                title: Text("Exit: Are you sure?"),
                 actions: <Widget>[
                   FlatButton(
                       onPressed: () => Navigator.pop(context, false),
