969284e31fb43ad96e8f5ff1b3e395bb57ec7ea3
anandu R <ananduraj32@gmail.com>
2020-03-23 19:46:11 +0530

    color meh

---
+++ lib/shared/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color bgColor = const Color(0xFFECECD7);
+Color coralColor = const Color(0xFFFF7E6B);
+Color raspColor = const Color(0xFF96616B);
+Color mintColor = const Color(0xFFA9F0D1);
+Color salmonColorh=const Color(0xFFFFA69E);
