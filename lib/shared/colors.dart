85bea3bcce1defc47c62c246dee770db7b19520c
anandu R <ananduraj32@gmail.com>
2020-03-23 20:13:31 +0530

    added colors easy usage import dart package color.dart

--- lib/shared/colors.dart
+++ lib/shared/colors.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 
 Color bgColor = const Color(0xFFECECD7);
 Color coralColor = const Color(0xFFFF7E6B);
-Color raspColor = const Color(0xFF96616B);
+Color raspberryColor = const Color(0xFF96616B);
 Color mintColor = const Color(0xFFA9F0D1);
 Color salmonColor = const Color(0xFFFFA69E);
 Color beige = const Color(0xFFEEF5DB);
 Color engVermillion = const Color(0xFFCC444B);
 Color pastelGrey = const Color(0xFFCBD4C2);
+Color darkSeaGreen = const Color(0xFF83BCA9);
+Color red = Colors.red[500];
+Color bgOrange = Colors.orange[100];
+Color buttonOrange = Colors.orange[500];
