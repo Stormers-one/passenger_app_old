d177e0462b2894eb7f928b0f8249db1b6c04a3ce
anandu R <ananduraj32@gmail.com>
2020-03-25 13:56:23 +0530

    checkpoint

--- lib/Menu/map.dart
+++ lib/Menu/map.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:o_k/shared/constants.dart';
 import 'package:o_k/shared/drawer.dart';
 import 'package:flutter_map/flutter_map.dart';
-// import 'package:flutter_map/plugin_api.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:livemap/livemap.dart';
+import 'package:latlong/latlong.dart';
+// import 'package:mapbox_gl/mapbox_gl.dart';
 
 class Map extends StatelessWidget {
   @override
@@ -15,19 +15,24 @@ class Map extends StatelessWidget {
         Container(
           child: FlutterMap(
             options: new MapOptions(
-              // center: LatLng(21.843,82.795), 
+              center: LatLng(21.843,82.795),
               zoom: 13.0,
             ),
             layers: [
               new TileLayerOptions(
-                  urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                  subdomains: ['a', 'b', 'c']),
+                  urlTemplate:
+                      "https://api.mapbox.com/styles/v1/aquaregis32/ck8720wdg0jk01illhroi1ogg/wmts?access_token=pk.eyJ1IjoiYXF1YXJlZ2lzMzIiLCJhIjoiY2s4NzFoYzBlMDQ5ZjNkbnljcnV4eTltayJ9.lxboNewyE7gaLHPQ4lFGtw",
+                  additionalOptions: {
+                    'accessToken':
+                        'pk.eyJ1IjoiYXF1YXJlZ2lzMzIiLCJhIjoiY2s4NzFoYzBlMDQ5ZjNkbnljcnV4eTltayJ9.lxboNewyE7gaLHPQ4lFGtw',
+                    'id': 'mapbox.mapbox-streets-v8',
+                  }),
               new MarkerLayerOptions(
                 markers: [
                   new Marker(
                     width: 80.0,
                     height: 80.0,
-                    // point: LatLng(21.843,82.795),
+                    point: LatLng(21.843,82.795),
                     builder: (ctx) => new Container(
                       child: new FlutterLogo(),
                     ),
