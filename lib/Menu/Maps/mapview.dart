94d2a1b653dff9cbc20541e2c3ad981bf337ab01
Anandu R <ananduraj32@gmail.com>
2020-03-27 05:58:38 +0530

    mapbox tinkering

--- lib/Menu/Maps/mapview.dart
+++ lib/Menu/Maps/mapview.dart
@@ -1,3 +1,7 @@
+// Kerala City limits : 
+// southwest: LatLng(7.962818, 74.949406),
+// northeast: LatLng(11.879561, 77.688881),
+
 library mapbox_gl;
 import 'package:flutter/material.dart';
 import 'package:o_k/shared/colors.dart';
@@ -8,112 +12,104 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:meta/meta.dart';
 
-part 'src/controller.dart';
-part 'src/flutter_mapbox_view.dart';
-part 'src/camera.dart';
-part 'src/models.dart';
+// part 'src/controller.dart';
+// part 'src/flutter_mapbox_view.dart';
+// part 'src/camera.dart';
+// part 'src/models.dart';
 
-// class MapView extends StatefulWidget {
-//   @override
-//   _MapView createState() => _MapView();
-// }
+class MapView extends StatefulWidget {
+  @override
+  _MapView createState() => _MapView();
+}
 
-// class _MapView extends State<MapView> {
-//   final CameraPosition _kInitialPosition;
-//   final CameraTargetBounds _cameraTargetBounds;
-//   static double defaultZoom = 12.0;
+class _MapView extends State<MapView> {
+  final CameraPosition _kInitialPosition;
+  final CameraTargetBounds _cameraTargetBounds;
+  static double defaultZoom = 8.0;
 
-//   CameraPosition _position;
-//   MapboxMapController mapController;
-//   bool _isMoving = false;
-//   bool _compassEnabled = true;
-//   MinMaxZoomPreference _minMaxZoomPreference =
-//       const MinMaxZoomPreference(12.0, 18.0);
-//   String _styleString = "mapbox://styles/mapbox/streets-v8";
-//   bool _rotateGesturesEnabled = true;
-//   bool _scrollGesturesEnabled = true;
-//   bool _tiltGesturesEnabled = false;
-//   bool _zoomGesturesEnabled = true;
-//   bool _myLocationEnabled = false;
-//   MyLocationTrackingMode _myLocationTrackingMode = MyLocationTrackingMode.None;
+  CameraPosition _position;
+  MapboxMapController mapController;
+  bool _isMoving = false;
+  bool _compassEnabled = true;
+  MinMaxZoomPreference _minMaxZoomPreference =
+      const MinMaxZoomPreference(4.0, 18.0);
+  String _styleString = "mapbox://styles/mapbox/streets-v8";
+  bool _rotateGesturesEnabled = true;
+  bool _scrollGesturesEnabled = true;
+  bool _tiltGesturesEnabled = false;
+  bool _zoomGesturesEnabled = true;
+  bool _myLocationEnabled = false;
+  MyLocationTrackingMode _myLocationTrackingMode = MyLocationTrackingMode.None;
 
-//   _MapView._(
-//       this._kInitialPosition, this._position, this._cameraTargetBounds);
+  _MapView._(
+      this._kInitialPosition, this._position, this._cameraTargetBounds);
 
-//   static CameraPosition _getCameraPosition() {
-//     final latLng = LatLng(40.7864, 0.2065);
-//     return CameraPosition(
-//       target: latLng,
-//       zoom: defaultZoom,
-//     );
-//   }
+  static CameraPosition _getCameraPosition() {
+    final latLng = LatLng(9.959624, 76.335317);
+    return CameraPosition(
+      target: latLng,
+      zoom: defaultZoom,
+    );
+  }
 
-//   factory _MapView() {
-//     CameraPosition cameraPosition = _getCameraPosition();
+  factory _MapView() {
+    CameraPosition cameraPosition = _getCameraPosition();
 
-//     // final cityBounds = LatLngBounds(
-//     //   southwest: LatLng(40.7413, -119.267),
-//     //   northeast: LatLng(40.8365, -119.1465),
-//     // );
+    final cityBounds = LatLngBounds(
+      southwest: LatLng(7.962818, 74.949406),
+      northeast: LatLng(11.879561, 77.688881),
+    );
 
-//     return _MapView._(
-//         cameraPosition, cameraPosition, CameraTargetBounds(cityBounds));
-//     // return _MapView._(
-//     //     cameraPosition, cameraPosition, CameraTargetBounds(cityBounds));
-//   }
+    return _MapView._(
+        cameraPosition, cameraPosition, CameraTargetBounds(cityBounds));
+  }
 
-//   void _onMapChanged() {
-//     setState(() {
-//       _extractMapInfo();
-//     });
-//   }
+  void _onMapChanged() {
+    setState(() {
+      _extractMapInfo();
+    });
+  }
 
-//   @override
-//   void dispose() {
-//     if (mapController != null) {
-//       mapController.removeListener(_onMapChanged);
-//     }
-//     super.dispose();
-//   }
+  @override
+  void dispose() {
+    if (mapController != null) {
+      mapController.removeListener(_onMapChanged);
+    }
+    super.dispose();
+  }
 
-//   void _extractMapInfo() {
-//     _position = mapController.cameraPosition;
-//     _isMoving = mapController.isCameraMoving;
-//   }
+  void _extractMapInfo() {
+    _position = mapController.cameraPosition;
+    _isMoving = mapController.isCameraMoving;
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       child: _buildMapBox(context),
-//     );
-//   }
+  @override
+  Widget build(BuildContext context) {
+    return MapboxMap(
+        onMapCreated: onMapCreated,
+        initialCameraPosition: this._kInitialPosition,
+        trackCameraPosition: true,
+        compassEnabled: _compassEnabled,
+        cameraTargetBounds: _cameraTargetBounds,
+        minMaxZoomPreference: _minMaxZoomPreference,
+        styleString: _styleString,
+        rotateGesturesEnabled: _rotateGesturesEnabled,
+        scrollGesturesEnabled: _scrollGesturesEnabled,
+        tiltGesturesEnabled: _tiltGesturesEnabled,
+        zoomGesturesEnabled: _zoomGesturesEnabled,
+        myLocationEnabled: _myLocationEnabled,
+        myLocationTrackingMode: _myLocationTrackingMode,
+        onCameraTrackingDismissed: () {
+          this.setState(() {
+            _myLocationTrackingMode = MyLocationTrackingMode.None;
+          });
+        });
+  }
 
-//   MapboxMap _buildMapBox(BuildContext context) {
-//     return MapboxMap(
-//         onMapCreated: onMapCreated,
-//         initialCameraPosition: this._kInitialPosition,
-//         trackCameraPosition: true,
-//         compassEnabled: _compassEnabled,
-//         cameraTargetBounds: _cameraTargetBounds,
-//         minMaxZoomPreference: _minMaxZoomPreference,
-//         styleString: _styleString,
-//         rotateGesturesEnabled: _rotateGesturesEnabled,
-//         scrollGesturesEnabled: _scrollGesturesEnabled,
-//         tiltGesturesEnabled: _tiltGesturesEnabled,
-//         zoomGesturesEnabled: _zoomGesturesEnabled,
-//         myLocationEnabled: _myLocationEnabled,
-//         myLocationTrackingMode: _myLocationTrackingMode,
-//         onCameraTrackingDismissed: () {
-//           this.setState(() {
-//             _myLocationTrackingMode = MyLocationTrackingMode.None;
-//           });
-//         });
-//   }
-
-//   void onMapCreated(MapboxMapController controller) {
-//     mapController = controller;
-//     mapController.addListener(_onMapChanged);
-//     _extractMapInfo();
-//     setState(() {});
-//   }
-// }
+  void onMapCreated(MapboxMapController controller) {
+    mapController = controller;
+    mapController.addListener(_onMapChanged);
+    _extractMapInfo();
+    setState(() {});
+  }
+}
