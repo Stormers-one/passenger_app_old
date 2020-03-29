814650cfad0b89d74a042c42765510d9dccf2f7b
Anandu R <ananduraj32@gmail.com>
2020-03-30 00:56:43 +0530

    Update booking.dart

--- lib/Menu/Booking/booking.dart
+++ lib/Menu/Booking/booking.dart
@@ -185,18 +185,31 @@ class _BookingState extends State<Booking> {
                                 //   ),
                                 // ]),
                                 Container(
+                                  padding: const EdgeInsets.only(
+                                      left: 20, right: 20),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      shape: BoxShape.rectangle,
+                                      border: Border.all(width: 1),
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(20.0),
+                                      )),
                                   child: TextFormField(
                                     controller: _controller2,
                                     textAlign: TextAlign.center,
                                     style: new TextStyle(color: Colors.black),
                                     onTap: () => _selectDate(context),
                                     decoration:
-                                        textInputDecoration("Date Of Departure"),
+                                        InputDecoration(
+                                      icon: new Icon(Icons.search),
+                                      hintText: "Date Of Departure",
+                                      border: InputBorder.none,
+                                    ),
                                     obscureText: false,
                                     keyboardType: TextInputType.text,
                                   ),
                                 ),
-                                
+
                                 new Padding(
                                   padding: const EdgeInsets.only(top: 30.0),
                                 ),
