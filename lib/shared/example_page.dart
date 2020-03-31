f39f59034b69d5af31f05a94ab71fe2dd11c4d6d
kenn850 <kennith.philips@gmail.com>
2020-04-01 02:02:03 +0530

    Under Development Thing

---
+++ lib/shared/example_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+//import 'package:url_launcher/url_launcher.dart';
+
+class ExamplePage extends StatefulWidget {
+  final String title;
+  final String pathToFile;
+  final WidgetBuilder builder;
+  final bool delayStartup;
+
+  ExamplePage(
+      {this.title, this.pathToFile, this.builder, this.delayStartup = false})
+      : assert(!pathToFile.contains("-"),
+            "Don't use minus character in filenames.");
+
+  @override
+  _ExamplePageState createState() => _ExamplePageState();
+}
+
+class _ExamplePageState extends State<ExamplePage> {
+  var renderBuilder = true;
+
+  @override
+  void initState() {
+    if (widget.delayStartup) {
+      renderBuilder = false;
+      Future.delayed(Duration(milliseconds: 500)).then((_) {
+        setState(() => renderBuilder = true);
+      });
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appbar(),
+      backgroundColor: Colors.orange[100],
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(child: _buildPage(context)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AppBar _appbar() {
+    return AppBar(
+      title: Text(widget.title),
+      actions: <Widget>[_reloadExampleButton(),],
+    );
+  }
+
+
+  IconButton _reloadExampleButton() {
+    return IconButton(
+      onPressed: () => setState(() {
+            renderBuilder = false;
+            Future.delayed(Duration(milliseconds: 200)).then((_) {
+              setState(() => renderBuilder = true);
+            });
+          }),
+      icon: Icon(
+        Icons.refresh,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Widget _buildPage(BuildContext context) {
+    if (!renderBuilder) {
+      return Container();
+    }
+    return this.widget.builder(context);
+  }
+}
