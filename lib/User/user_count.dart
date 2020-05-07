953d4269f5950784f5e9dd5de2f5f04f91b7142a
Anandu R <ananduraj32@gmail.com>
2020-05-07 15:26:17 +0530

    qrscan init, bluetooth complete(?)

--- lib/User/user_count.dart
+++ lib/User/user_count.dart
@@ -8,20 +8,18 @@ import 'package:o_k/shared/colors.dart';
 import 'package:provider/provider.dart';
 
 class UsersCount extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    void _showSettengsPanel() {
-      showModalBottomSheet(
-          context: context,
-          builder: (context) {
-            return Container(
-              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
-              child: SettingsForm(),
-            );
-          });
-    }
-
+    // void _showSettengsPanel() {
+    //   showModalBottomSheet(
+    //       context: context,
+    //       builder: (context) {
+    //         return Container(
+    //           padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+    //           child: SettingsForm(),
+    //         );
+    //       });
+    // }
     return StreamProvider<List<Users>>.value(
       value: DatabaseService().users,
       child: MaterialApp(
@@ -32,12 +30,13 @@ class UsersCount extends StatelessWidget {
               elevation: 0,
               title: Text('Passengers'),
               backgroundColor: red,
-              actions: <Widget>[
-                FlatButton.icon(
-                    onPressed: () => _showSettengsPanel(),
-                    icon: Icon(Icons.settings),
-                    label: Text('Edit Profile'))
-              ]),
+              // actions: <Widget>[
+              //   FlatButton.icon(
+              //       onPressed: () => _showSettengsPanel(),
+              //       icon: Icon(Icons.settings),
+              //       label: Text('Edit Profile'))
+              // ],
+            ),
           drawer: DrawerBuild(),
           body: UserList(),
         ),
--- lib/conductor/Bluetooth.dart
+++ lib/conductor/Bluetooth.dart
@@ -1,9 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:o_k/shared/drawer.dart';
 import 'package:o_k/shared/colors.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 
-class Bluetooth extends StatelessWidget {
+class Bluetooth extends StatefulWidget{
+  Bluetooth({Key key,this.title}) : super(key:key);
+  final FlutterBlue flutterBlue = FlutterBlue.instance;
+  final List<BluetoothDevice> devicesList = new List<BluetoothDevice>();
+  final String title;
+  @override
+  _BluetoothState createState() => _BluetoothState();
+}
+class _BluetoothState extends State<Bluetooth> {
+  @override 
+  void initState() {
+   super.initState();
+   widget.flutterBlue.connectedDevices
+       .asStream()
+       .listen((List<BluetoothDevice> devices) {
+     for (BluetoothDevice device in devices) {
+       _addDeviceTolist(device);
+     }
+   });
+   widget.flutterBlue.scanResults.listen((List<ScanResult> results) {
+     for (ScanResult result in results) {
+       _addDeviceTolist(result.device);
+     }
+   });
+   widget.flutterBlue.startScan();
+ }
+
+ ListView _buildListViewOfDevices() {
+   List<Container> containers = new List<Container>();
+   for (BluetoothDevice device in widget.devicesList) {
+     containers.add(
+       Container(
+         height: 50,
+         child: Row(
+           children: <Widget>[
+             Expanded(
+               child: Column(
+                 children: <Widget>[
+                   Text(device.name == '' ? '(unknown device)' : device.name),
+                   Text(device.id.toString()),
+                 ],
+               ),
+             ),
+             FlatButton(
+               color: Colors.blue,
+               child: Text(
+                 'Connect',
+                 style: TextStyle(color: Colors.white),
+               ),
+               onPressed: () {},
+             ),
+           ],
+         ),
+       ),
+     );
+   }
+ 
+   return ListView(
+     padding: const EdgeInsets.all(8),
+     children: <Widget>[
+       ...containers,
+     ],
+   );
+ }
+
+  _addDeviceTolist(final BluetoothDevice device) {
+   if (!widget.devicesList.contains(device)) {
+     setState(() {
+       widget.devicesList.add(device);
+     });
+   }
+ }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,6 +93,15 @@ class Bluetooth extends StatelessWidget {
           onTap: () {
             FocusScope.of(context).requestFocus(new FocusNode());
           },
+          child: SafeArea(
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  _buildListViewOfDevices()
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
